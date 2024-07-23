ef2c0f2a39e27a2ba05cc743fca0342dc6d24096
affan <affan@propertyguru.com.sg>
2024-07-23 22:43:04 +0800

    Use MUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 export default function Home() {
   return (
-    <main>
+    <main className={roboto.className}>
       <div className="hello">Hello world!</div>
     </main>
   );
